9f85732fe5cc867837bd22b0542d46b80108c069
Sachin Maharjan <sachin.maharjan@bajratechnologies.com>
2025-12-14 16:15:03 +0545

    feat:  initialize devise with user model

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+# User
+class User < ApplicationRecord
+  include DeviseTokenAuth::Concerns::User
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
+  # DeviseTokenAuth expects this method
+  def confirmed?
+    true
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users, skip: %i[registrations sessions] # Skip default routes
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      devise_scope :user do
+        post 'users/signup', to: 'users/registrations#create'
+        post 'auth/signin',  to: 'users/sessions#create'
+      end
+    end
+  end
   # Defines the root path route ("/")
   root 'welcome#index'
 end
